5dcc8f42be8885cbeff754f563a6998d26c77399
naroors <oliwernoga@onet.pl>
2024-05-22 21:57:44 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+function openMenu() {
+    var menu = document.querySelector('.menu');
+    var burgerMenu = document.querySelector('.burger-menu');
+    
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+        burgerMenu.style.display = 'block';
+    } else {
+        menu.style.display = 'block';
+        burgerMenu.style.display = 'none';
+    }
+}
 // naroors
